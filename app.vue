55e064b69bfdfde7ae0ab7d06dbf7564fecc1058
Alex <self@pirsig.net>
2023-06-08 18:56:35 +0200

    reduce to a simpler example

--- app.vue
+++ app.vue
@@ -2,24 +2,20 @@
 // @ts-expect-error avoid lint error
 import markdownParser from '@nuxt/content/transformers/markdown'
 
-export interface MarkdownRenderProps {
-  md?: string
-  cid?: string
-}
-
-const markdownMarkUp = `
+const markdown = `
 
 ## hello World
-
 
 Hello this is markdown
 `
 
-const props = withDefaults(defineProps<MarkdownRenderProps>(), { md: '', cid: 'helloWorld' })
-const parsedMarkdown = await markdownParser.parse(props.cid, markdownMarkUp)
+const parsedMarkdown = await markdownParser.parse('helloWorld', markdown)
 
 </script>
 
 <template>
-      <ContentRendererMarkdown :value="parsedMarkdown" />
+  Markdown manually rendered 👇 <br /> 
+  <pre>
+    <ContentRendererMarkdown :value="parsedMarkdown" />
+  </pre>
 </template>
